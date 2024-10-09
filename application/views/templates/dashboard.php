800d8eb79cb08309ba8bbf14e5ed51dcccc12341
unknown <doniwicaksono27@gmail.com>
2024-10-09 08:39:31 +0700

    row tabel jadi 25

--- application/views/templates/dashboard.php
+++ application/views/templates/dashboard.php
@@ -422,6 +422,26 @@
             cb(start, end);
         });
 
+        // $(document).ready(function() {
+        //     var table = $('#dataTable').DataTable({
+        //         buttons: ['copy', 'print', 'excel', 'pdf'],
+        //         dom: "<'row px-2 px-md-4 pt-2'<'col-md-3'l><'col-md-5 text-center'B><'col-md-4'f>>" +
+        //             "<'row'<'col-md-12'tr>>" +
+        //             "<'row px-2 px-md-4 py-3'<'col-md-5'i><'col-md-7'p>>",
+        //         lengthMenu: [
+        //             [5, 10, 25, 50, 100, -1],
+        //             [5, 10, 25, 50, 100, "All"]
+        //         ],
+        //         columnDefs: [{
+        //             targets: -1,
+        //             orderable: false,
+        //             searchable: false
+        //         }]
+        //     });
+
+        //     table.buttons().container().appendTo('#dataTable_wrapper .col-md-5:eq(0)');
+        // });
+
         $(document).ready(function() {
             var table = $('#dataTable').DataTable({
                 buttons: ['copy', 'print', 'excel', 'pdf'],
@@ -429,8 +449,8 @@
                     "<'row'<'col-md-12'tr>>" +
                     "<'row px-2 px-md-4 py-3'<'col-md-5'i><'col-md-7'p>>",
                 lengthMenu: [
-                    [5, 10, 25, 50, 100, -1],
-                    [5, 10, 25, 50, 100, "All"]
+                    [ 25, 50, 100, -1],
+                    [ 25, 50, 100, "All"]
                 ],
                 columnDefs: [{
                     targets: -1,
